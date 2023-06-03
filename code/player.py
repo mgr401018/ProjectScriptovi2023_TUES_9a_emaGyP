ee431e47c0f69866fa26d8f39d21d0ba45262e4a
HyperRaptor <103509051+HyperRaptor@users.noreply.github.com>
2023-06-03 20:13:07 +0300

    Adding temporary map (might become permanent)

--- code/player.py
+++ code/player.py
@@ -4,11 +4,11 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups,obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('.\graphics\character.png').convert_alpha()
+        self.image = pygame.image.load('.\\graphics\\character.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-26)
         self.direction = pygame.math.Vector2()
-        self.speed = 5
+        self.speed = 10
         self.obstacle_sprites = obstacle_sprites
 
     def input(self):
